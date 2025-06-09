446f1ae345600c623791c4aa7ac89ff0274043b8
pjs6203 <pjs6203@gmail.com>
2025-06-09 19:44:30 +0900

    이동 함수 수정

--- arduinoRobot/arduinoRobot.ino
+++ arduinoRobot/arduinoRobot.ino
@@ -1,29 +1,4 @@
 /*
-[moving.ino]
-
-updateOdom() //로봇 위치 추적
-moveStraight(float mm, int spdDeg = 360) //전후진
-turnInPlace(float deg, int spdDeg = 300) //회전
-moveArc(float R_mm, float theta_deg, int spdDeg = 300) //원호주행
-moveStraightUntil(int16_t speedDeg, ValueFn curValFn, CmdOp op, float refVal) //전후진 중 특정 조건 도달 시 정지
-setFirstPose() //로봇의 초기 위치 설정
-*/
-
-/*
-[sensor_vl53l0x.ino]
-
-distanceSensor_init() //vl53l0x I2C 주소 재설정 및 파라미터 설정
-readDistanceSensorFront() //전방 거리센서값 읽기
-readDistanceSensorRight() //우측 거리센서값 읽기
-*/
-
-/*
-[sensor_huskylens.ino]
-
-readQRdata() //QR인식
-*/
-
-/*
 [I2C Address]
 PRIZM : 0x01 ~ 0x06 (prizm programming guide 134P 참고)
 frontToF (VL53L0X) : 0x29 -> 0x30
@@ -39,17 +14,6 @@ HUSKYLENS : 0x32
 5구역 : 2600, 1200
 6구역 : 3050, 1200
 시작/종료 구역 : 3150, 250
-*/
-
-/*
-1. Huskylens QR, 팔레트 학습시키기 (참고 : https://blog.naver.com/icbanq/223079420765)
-2. setMotorInvert 하면 엔코더 값도 음수로 들어오는지 확인해야함 >> 음수로 들어옴
-3. 휠 간 거리 측정
-4. 센서 거리 측정
-5. prizm.PrizmEnd 주석풀기
-6. 거리 오차 테스트
-7. 가속도 ?
-8. ToF XSHUT 핀들 프리즘에 맞게 다시 써야함
 */
 
 //moveStraightUntil
@@ -72,9 +36,6 @@ enum CmdOp { GE, LE, GT, LT, EQ };
 #include <Wire.h>
 #include <Thread.h> //ivanseidel
 #include <ThreadController.h> //ivanseidel
-
-
-
 
 
 //VL53L0X
@@ -100,9 +61,9 @@ volatile float x_mm = 0, y_mm = 0, th_deg = 0;
 long prevL=0, prevR=0;
 
 /* ─── 하드웨어 파라미터 ───────────────────── */
-const float WHEEL_D   = 100;               // 휠 지름 [mm]
+const float WHEEL_D   = 105;               // 휠 지름 [mm]
 const float WHEEL_C   = PI * WHEEL_D;        // 휠 거리 [mm]
-const float WHEEL_BASE= 100.0;               // 휠 간 거리 [mm] 확인 후 바꿔야함
+const float WHEEL_BASE= 355;               // 휠 간 거리 [mm] 확인 후 바꿔야함
 const int   TICKS_REV = 1440;                // 1회전 당 엔코더 틱 [ticks/rev]
 const float MM_PER_TICK = WHEEL_C / TICKS_REV;
 const int   world_X = 3400;
@@ -515,9 +476,8 @@ void setup()
 
   //PRIZM Board
   prizm.PrizmBegin();
-  prizm.setMotorInvert(2,1); //2번 모터를 반
-
-  //prizm.resetEncoders();
+  prizm.setMotorInvert(2,1); //2번 모터를 반전
+  prizm.resetEncoders();
 
   //I2C
   Wire.begin();
@@ -527,10 +487,6 @@ void setup()
 
   //VL53L0X
   distanceSensor_init(); 
-
-        
-
-
 
   //odometry thread
   odomThread.onRun(updateOdom);
@@ -558,8 +514,9 @@ void loop()
   // moveStraight(mm, speed(-720 ~ 720))
   // turnInPlace(deg, deg/s(-720 ~ 720))
   
-  moveStraight(1000, 360, 0.35);
-  moveStraightUntil(360, readDistanceSensorFront(), LE, 500, 0.2);
+  //moveStraight(1000, 720, 0.35);
+  //moveStraightUntil(360, readDistanceSensorFront(), LE, 500, 0.2);
+  //turnInPlace(270, 360, 0.2);
 
 /*
   // 1) 엔코더 값 리셋 및 상태 초기화
@@ -629,6 +586,6 @@ Serial.print(prizm.readEncoderCount(1));
   turnInPlace(90, 720);
 
 */
-  //prizm.PrizmEnd();  //나중에 활성화하기
+  prizm.PrizmEnd();  //나중에 활성화하기
 
 }
